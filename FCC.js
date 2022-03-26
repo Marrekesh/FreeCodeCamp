0e0c0558ec7d8510153075b0edfee1fbac00e4cb
dima doronin <warreaver123@gmail.com>
2022-03-26 11:29:07 +0200

    1/5 basic js

--- FCC.js
+++ FCC.js
@@ -1,4 +1,4 @@
-//FREECODECAMP
+//FREECODECAMP BASIC
 
 // create variable
 var myName;
@@ -12,3 +12,126 @@ var a;
 a = 7;
 var b;
 b = a
+
+//Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Modify the existing declarations and assignments so their names use camelCase.
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Update the code so it only uses the let keyword.
+
+var catName = "Oliver";
+var catSound = "Meow!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+
+// Change the code so that all variables are declared using let or const
+
+const fCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(fCC, fact); 
+
+// Измените 0так, чтобы сумма была равна 20.
+
+const sum = 10 + 10;
+
+// Change the 0 so the difference is 12.
+
+const difference = 45 - 33;
+
+// Change the 0 so that product will equal 80.
+
+const product = 8 * 10;
+
+// Change the 0 so that the quotient is equal to 2.
+
+const quotient = 66 / 33;
+
+// Change the code to use the ++ operator on myVar.
+
+let myVar = 87;
+
+myVar++;
+
+// Change the code to use the -- operator on myVar.
+
+let myVariable = 11;
+
+// Only change code below this line
+myVariable--;
+
+// Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+const myDecimal = 6.1;
+
+// Change the 0.0 so that product will equal 5.0.
+
+const productN = 2.0 * 2.5;
+
+//Change the 0.0 so that quotient will equal to 2.2.
+
+const quotientN = 4.4 / 2.0;
+
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+const remainder = 11 % 3;
+
+// Convert the assignments for a, b, and c to use the += operator.
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+// Convert the assignments for a, b, and c to use the -= operator.
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+// Convert the assignments for a, b, and c to use the *= operator.
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+// Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
