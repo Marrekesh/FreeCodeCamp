324b0d456a63202520cc1d1c90fbcd4024708258
dima doronin <warreaver123@gmail.com>
2022-03-26 20:39:28 +0200

    80% of basic js

--- FCC.js
+++ FCC.js
@@ -363,3 +363,66 @@ function nextInLine(arr, item) {
   }
   
   chainToSwitch(7);
+
+
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch(card) {
+    case 2 :
+    case 3 :
+    case 4 :
+    case 5 :
+    case 6 :
+      count += 1;
+      break;
+    case 7 :
+    case 8 :
+    case 9 :
+      count += 0;
+      break;
+    case 10 :
+    case 'J' :
+    case 'Q' :
+    case 'K' :
+    case 'A' :
+      count -= 1;
+      break;
+  }
+
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold"
+  }
+  
+  // Only change code above this line
+}
+
+console.log(cc(10))
+console.log(cc('J'))
+console.log(cc('Q'))
+console.log(cc('K'))
+console.log(cc('A'))
+//10, J, Q, K, A
+
+// Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+function phoneticLookup(val) {
+    let result = "";
+  
+  
+    const lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank",
+    }
+  
+    result = lookup[val]
+    // Only change code above this line
+    return result;
+  }
