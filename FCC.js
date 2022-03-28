6c9838680e0b1a42009153334d7584dd0d867477
dima doronin <warreaver123@gmail.com>
2022-03-28 11:57:09 +0300

    100% of basic js

--- FCC.js
+++ FCC.js
@@ -510,3 +510,176 @@ const myArrays = [];
 for (let i = 1; i < 10; i += 2) {
   myArrays.push(i);
 }
+
+// Declare and initialize a variable total to 0. Use a for loop to add the value of each element of the myArr array to total.
+
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+// Only change code below this line
+let total = 0
+
+for (let i = 0; i < myArr.length; i++) {
+  total += myArr[i]
+}
+
+//Change the while loop in the code to a do...while loop so the loop will push only the number 10 to myArray, and i will be equal to 11 when your code has finished running.
+
+
+// Setup
+const myArrayq = [];
+let i = 10;
+
+// Only change code below this line
+
+do {
+  myArrayq.push(i)
+  i++
+} while (i < 10)
+
+// Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+
+function sum(arr, n) {
+  // Only change code below this line
+  if (n <= 0) {
+    return 0
+  } else {
+    return sum(arr, n - 1) + arr[n - 1];
+  }
+  // Only change code above this line
+}
+
+
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+  let match = false
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i]['firstName'] === name) {
+      match = true
+      if (!contacts[i][prop]) {
+        return 'No such property'
+      } else {
+        return contacts[i][prop]
+      }
+    } 
+  }
+
+  if (!match) {
+    return 'No such contact'
+  } 
+
+  // Only change code above this line
+}
+
+console.log(lookUpProfile("Kristian", "bebera"));
+
+//Use this technique to generate and return a random whole number between 0 and 9.
+
+function randomWholeNum() {
+
+  // Only change code below this line
+
+  return Math.floor(Math.random() * 10);
+}
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+// Create a function called randomRange that takes a range myMin and myMax and returns a random whole number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+
+function randomRange(myMin, myMax) {
+  // Only change code below this line
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+  // Only change code above this line
+}
+
+// Use parseInt() in the convertToInteger function so it converts the input string str into an integer, and returns it.
+
+function convertToInteger(str) {
+  return parseInt(str)
+ }
+ 
+ convertToInteger("56");
+
+ //Use parseInt() in the convertToInteger function so it converts a binary number to an integer and returns it.
+
+ function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
+
+// Use the conditional operator in the checkEqual function to check if two numbers are equal or not. The function should return either the string Equal or the string Not Equal.
+
+function checkEqual(a, b) {
+  return a == b ? 'Equal' : 'Not Equal'
+}
+
+checkEqual(1, 2);
+
+//In the checkSign function, use multiple conditional operators - following the recommended format used in findGreaterOrEqual - to check if a number is positive, negative or zero. The function should return positive, negative or zero.
+
+function checkSign(num) {
+  return (num > 0) ? 'positive'
+    : (num < 0) ? 'negative'
+    : 'zero'
+}
+
+checkSign(10);
+
+
+function countdown(n) {
+  if (n < 1) {
+    return []
+  } else {
+    const array = countdown(n - 1)
+    array.unshift(n)
+    return array
+  }
+  
+}
+
+console.log(countdown(10))
+
+// Continuing from the previous challenge, we provide you another opportunity to create a recursive function to solve a problem.
+
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum > endNum) {
+  return []
+} else {
+  const array = rangeOfNumbers(startNum, endNum - 1)
+  array.push(endNum)
+  return array
+}
+};
+
+console.log(rangeOfNumbers(1, 5))
