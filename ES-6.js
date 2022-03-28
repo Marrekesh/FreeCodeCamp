b971538c48d3f56d9a4e62f96931e20de675754f
dima doronin <warreaver123@gmail.com>
2022-03-28 22:34:02 +0300

    40% of ES6 js

--- ES-6.js
+++ ES-6.js
@@ -75,3 +75,33 @@ const HIGH_TEMPERATURES = {
   
   const { today, tomorrow} = HIGH_TEMPERATURES
 
+  //Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables highToday and highTomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+  const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+    
+   const { today: highToday , tomorrow: highTomorrow } = HIGH_TEMPERATURES
+  
+
+// Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables lowToday and highToday the values of today.low and today.high from the LOCAL_FORECAST object.
+
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+
+const { today:{low: lowToday, high: highToday} } = LOCAL_FORECAST
+
+
+//Use destructuring assignment to swap the values of a and b so that a receives the value stored in b, and b receives the value stored in a.
+let a = 8, b = 6;
+// Only change code below this line
+
+[b, a] = [a, b]
+
+console.log(b, a)
