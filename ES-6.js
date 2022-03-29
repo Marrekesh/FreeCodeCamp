36ca6864b32b053a64ff079754db034e96874443
dima doronin <warreaver123@gmail.com>
2022-03-29 11:22:18 +0300

    50% of ES6 js

--- ES-6.js
+++ ES-6.js
@@ -105,3 +105,50 @@ let a = 8, b = 6;
 [b, a] = [a, b]
 
 console.log(b, a)
+
+//Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() so that arr is a sub-array of the original array source with the first two elements omitted.
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  const  [a, b, ...arr] = list; 
+
+  return arr;
+}
+const arr = removeFirstTwo(source);
+
+//Use destructuring assignment within the argument to the function half to send only max and min inside the function.
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+
+const half = ({max, min}) => (max + min) / 2.0; 
+
+// Use template literal syntax with backticks to create an array of list element (li) strings. Each list element's text should be one of the array elements from the failure property on the result object and have a class attribute with the value text-warning. The makeList function should return the array of list item strings.
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+
+
+function makeList(arr) {
+  // Only change code below this line
+
+  const failureItems = arr.map((item) => {
+    return (
+      `<li class="text-warning">${item}</li>`
+    )
+  })
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
