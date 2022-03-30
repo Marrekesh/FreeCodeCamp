aa7153e34c8f1470e915f02302fa00c4c88150bc
dima doronin <warreaver123@gmail.com>
2022-03-30 19:15:21 +0300

    10% of regexp

---
+++ RegExp.js
@@ -0,0 +1,10 @@
+// Apply the regex myRegex on the string myString using the .test() method.
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); 
+
+// Complete the regex petRegex to match the pets dog, cat, bird, or fish.
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
