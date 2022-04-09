85bc091d6fb9106993d64485370a2e0287bafad8
dima doronin <warreaver123@gmail.com>
2022-04-09 13:58:13 +0300

    30% of debug

---
+++ debug.js
@@ -0,0 +1,29 @@
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+console.log(a)
+
+let sumAB = a + b;
+console.log(sumAB);
+
+// После этого используйте console.logдля регистрации outputпеременной. Просмотрите две консоли, чтобы увидеть журнал. Наконец, используйте console.clearпосле вашего журнала, чтобы очистить консоль браузера. Посмотрите разницу в двух консолях.
+
+let output = "Get this to show once in the freeCodeCamp console and not at all in the browser console";
+console.log(output)
+console.clear()
+
+// Добавьте два console.log()оператора для проверки typeofкаждой из двух переменных sevenи threeв коде.
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+
+console.log(typeof seven)
+console.log(typeof three)
+
+// Исправьте две орфографические ошибки в коде, чтобы netWorkingCapitalвычисления работали.
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
